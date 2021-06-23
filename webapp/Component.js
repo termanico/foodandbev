a1b21b2e16106109a7a729fce140e8d3169bab46
termanico <klingbeil.thorsten@t-online.de>
2021-06-23 12:11:02 +0200

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("sap.fe.demo.foodandbev.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
